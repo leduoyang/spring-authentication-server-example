11aa3dbbd7e3018adade2a448787e11fa1cc0ffe
leduoyang <leduoyoung@gmail.com>
2022-09-13 16:16:28 +0800

    update examples

---
+++ api-client-example/src/test/java/com/example/apiclientexample/ApiClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
